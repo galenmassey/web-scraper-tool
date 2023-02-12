30d0ee080ea77686b3f6c855b44777a1a44efd41
Dius Montenegro <114764373+DreaUltimate@users.noreply.github.com>
2023-02-12 21:20:08 +0800

    add code comments

--- web-scraper-tool.py
+++ web-scraper-tool.py
@@ -1,22 +1,32 @@
+#importing necessary libraries
 import requests
 from bs4 import BeautifulSoup
 
+#defining the function to scrape the web page 
 def scrape_web(url):
 
+    #making a request to the url and getting the response 
     response = requests.get(url)
 
+    #parsing the response using BeautifulSoup 
     soup = BeautifulSoup(response.text, 'html.parser')
 
+    #finding all the elements with tag 'a' 
     links = soup.find_all('a')
 
+    #creating an empty list to store all the scraped data 
     data = []
 
+    #looping through each link and extracting required information 
     for link in links:
 
+        #extracting href attribute from each link 
         href = link.get('href')
- 
+
+        #extracting text from each link 
         text = link.text
 
+        #appending extracted data into list 
         data.append({'href': href, 'text': text})
 
     return data
